f930f5c3279aaf732dbd1be8eea209e4aef311b0
Sahuvivek12 <viveksahu818@gmail.com>
2024-10-10 00:35:40 +0530

    commit

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from conifg import KEYS
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config[KEYS.SECRET_KEY]
+    app.config['SECRET_KEY'] = os.getenv('key')
 
     return app
